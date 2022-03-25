b8f5e06df34f1357af48d65f124c55a4f7efaa83
unknown <priyanka.rahate23@gmail.com>
2022-03-25 17:33:11 +0530

    Reverse

---
+++ src/ReverseNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseNum {
+
+	public static void main(String[] args) {
+		int n,r;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter any number");
+		n=s.nextInt();
+		
+		while(n>0)
+		{
+			r=n%10;
+			System.out.print(r);
+			n=n/10;
+		
+		}
+
+	}
+
+
+	}
+
+
